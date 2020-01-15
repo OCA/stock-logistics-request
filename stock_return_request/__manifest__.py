7ae38dbef488b8e2e7cdd08bc9236de44b644ab1
david <david.vidal@tecnativa.com>
2020-01-15 15:45:06 +0100

    [IMP] stock_return_request: lot suggestions

--- stock_return_request/__manifest__.py
+++ stock_return_request/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Stock Return Request",
-    "version": "11.0.1.0.0",
+    "version": "11.0.1.1.0",
     "category": "Stock",
     "website": "https://github.com/OCA/stock-logistics-workflow",
     "author": "Tecnativa, "
@@ -18,6 +18,7 @@
         'data/stock_return_request_data.xml',
         'views/stock_return_request_views.xml',
         'report/stock_return_report.xml',
+        'wizard/suggest_return_request.xml',
     ],
     "pre_init_hook": "pre_init_hook",
     "post_init_hook": "post_init_hook",
