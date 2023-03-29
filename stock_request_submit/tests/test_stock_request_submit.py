1ea5f677884f19509abb349381d5c6d77816dd8e
Urvisha-OSI <udesai@opensourceintegrators.com>
2023-03-29 14:23:22 +0530

    [MIG] stock_request_submit: Migration to 16.0
    TT47868
    
    Co-authored-by: Urvisha-OSI

--- stock_request_submit/tests/test_stock_request_submit.py
+++ stock_request_submit/tests/test_stock_request_submit.py
@@ -2,92 +2,21 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
 
 from odoo import fields
-from odoo.tests.common import TransactionCase
+
+from odoo.addons.stock_request.tests.test_stock_request import TestStockRequest
 
 from ..hooks import uninstall_hook
 
 
-class TestStockRequestSubmit(TransactionCase):
+class TestStockRequestSubmit(TestStockRequest):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-
-        # Model
-        cls.StockRequest = cls.env["stock.request"]
-        cls.StockRequestOrder = cls.env["stock.request.order"]
-        cls.StockLocation = cls.env["stock.location"]
-        cls.StockLocationRoute = cls.env["stock.location.route"]
-        cls.StockRule = cls.env["stock.rule"]
-        cls.ProductProduct = cls.env["product.product"]
-        cls.ResUsers = cls.env["res.users"]
-
-        # Data
-        cls.expected_date = fields.Datetime.now()
-        cls.main_company = cls.env.ref("base.main_company")
-        cls.warehouse = cls.env.ref("stock.warehouse0")
-        cls.stock_request_user_group = cls.env.ref(
-            "stock_request.group_stock_request_user"
-        )
-        cls.stock_request_manager_group = cls.env.ref(
-            "stock_request.group_stock_request_manager"
-        )
-        cls.ressuply_loc = cls.StockLocation.create(
-            {
-                "name": "Ressuply",
-                "location_id": cls.warehouse.view_location_id.id,
-                "usage": "internal",
-                "company_id": cls.main_company.id,
-            }
-        )
-        cls.route = cls.StockLocationRoute.create(
-            {
-                "name": "Transfer",
-                "product_categ_selectable": False,
-                "product_selectable": True,
-                "company_id": cls.main_company.id,
-                "sequence": 10,
-            }
-        )
-        cls.rule = cls.StockRule.create(
-            {
-                "name": "Transfer",
-                "route_id": cls.route.id,
-                "location_src_id": cls.ressuply_loc.id,
-                "location_id": cls.warehouse.lot_stock_id.id,
-                "action": "pull",
-                "picking_type_id": cls.warehouse.int_type_id.id,
-                "procure_method": "make_to_stock",
-                "warehouse_id": cls.warehouse.id,
-                "company_id": cls.main_company.id,
-            }
-        )
-        cls.product = cls.ProductProduct.create(
-            {
-                "name": "test product",
-                "type": "product",
-                "route_ids": [(6, 0, cls.route.ids)],
-            }
-        )
-        cls.stock_request_user = cls.ResUsers.create(
-            {
-                "name": "Stock Request User",
-                "login": "stock_request_user",
-                "groups_id": [(6, 0, [cls.stock_request_user_group.id])],
-            }
-        )
-        cls.stock_request_manager = cls.ResUsers.create(
-            {
-                "name": "Stock Request Manager",
-                "login": "stock_request_manager",
-                "groups_id": [(6, 0, [cls.stock_request_manager_group.id])],
-            }
-        )
-
         vals = {
             "company_id": cls.main_company.id,
             "warehouse_id": cls.warehouse.id,
             "location_id": cls.warehouse.lot_stock_id.id,
-            "expected_date": cls.expected_date,
+            "expected_date": fields.Datetime.now(),
             "stock_request_ids": [
                 (
                     0,
@@ -99,12 +28,12 @@ class TestStockRequestSubmit(TransactionCase):
                         "company_id": cls.main_company.id,
                         "warehouse_id": cls.warehouse.id,
                         "location_id": cls.warehouse.lot_stock_id.id,
-                        "expected_date": cls.expected_date,
+                        "expected_date": fields.Datetime.now(),
                     },
                 )
             ],
         }
-        cls.order = cls.StockRequestOrder.with_user(cls.stock_request_user).create(vals)
+        cls.order = cls.request_order.with_user(cls.stock_request_user).create(vals)
         cls.stock_request = cls.order.stock_request_ids
 
     def test_stock_request_submit(self):
