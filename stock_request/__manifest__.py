Change2improve/stock-logistics-warehouse

add stock_request in 11.0

stock_request/__manifest__.py
@@ -1,0 +1,23 @@
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Stock Request",
+    "summary": "Internal request for stock",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "website": "https://github.com/stock-logistics-warehouse",
+    "author": "Eficent, "
+              "Odoo Community Association (OCA)",
+    "category": "Warehouse Management",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "security/stock_request_security.xml",
+        "security/ir.model.access.csv",
+        "views/stock_request_view.xml",
+        "data/stock_request_sequence_data.xml"
+    ],
+    "installable": True,
+}

OCA/stock-logistics-request

stock_request/__manifest__.py
@@ -1,0 +1,26 @@
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Stock Request",
+    "summary": "Internal request for stock",
+    "version": "11.0.1.0.0",
+    "license": "LGPL-3",
+    "website": "https://github.com/stock-logistics-warehouse",
+    "author": "Eficent, "
+              "Odoo Community Association (OCA)",
+    "category": "Warehouse Management",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "security/stock_request_security.xml",
+        "security/ir.model.access.csv",
+        "views/stock_request_views.xml",
+        "views/stock_request_allocation_views.xml",
+        "views/stock_move_views.xml",
+        "views/stock_picking_views.xml",
+        "data/stock_request_sequence_data.xml"
+    ],
+    "installable": True,
+}