fe9547bcc77856bdd0ab146cff4452adecc53cc3
nicomacr <nmr@adhoc.com.ar>
2023-09-19 14:53:33 -0300

    [MIG] stock_request: Migration to 16.0

--- stock_request/__manifest__.py
+++ stock_request/__manifest__.py
@@ -4,9 +4,9 @@
 {
     "name": "Stock Request",
     "summary": "Internal request for stock",
-    "version": "15.0.1.6.3",
+    "version": "16.0.1.0.0",
     "license": "LGPL-3",
-    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "website": "https://github.com/OCA/stock-logistics-request",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "category": "Warehouse Management",
     "depends": ["stock"],
