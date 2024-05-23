450f1e4ec7908d6e8fd469868b65209129d4a5a3
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2024-05-23 12:38:53 +0530

    [IMP] stock_request_kanban: black, isort, prettier

--- stock_request_kanban/__manifest__.py
+++ stock_request_kanban/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Stock Request kanban",
     "version": "16.0.1.0.0",
     "category": "Warehouse Management",
-    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "website": "https://github.com/OCA/stock-logistics-request",
     "author": "Creu Blanca, ForgeFlow, Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "summary": "Adds a stock request order, and takes stock requests as lines",
