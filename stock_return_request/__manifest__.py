5319b64a3183a5d73e5dbe841a8fc7ddb8996280
Anusha <anusha.pp@calin.co.in>
2026-06-03 06:56:47 +0200

    [IMP] stock_return_request: pre-commit auto fixes

--- stock_return_request/__manifest__.py
+++ stock_return_request/__manifest__.py
@@ -5,7 +5,7 @@
     "version": "18.0.1.0.1",
     "category": "Stock",
     "website": "https://github.com/OCA/stock-logistics-request",
-    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
