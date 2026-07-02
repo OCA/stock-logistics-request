ad2e7c16fab6227d0eca6b9eba5da3124981cbd3
LauraCForgeFlow <laura.cazorla@forgeflow.com>
2026-07-02 07:56:43 +0200

    [FIX] stock_request: sudo access errors

--- stock_request/models/stock_location.py
+++ stock_request/models/stock_location.py
@@ -12,7 +12,9 @@ class StockLocation(models.Model):
     def _check_company_stock_request(self):
         if any(
             rec.company_id
-            and self.env["stock.request"].search(
+            and self.env["stock.request"]
+            .sudo()
+            .search(
                 [("company_id", "!=", rec.company_id.id), ("location_id", "=", rec.id)],
                 limit=1,
             )
@@ -27,7 +29,9 @@ class StockLocation(models.Model):
             )
         if any(
             rec.company_id
-            and self.env["stock.request.order"].search(
+            and self.env["stock.request.order"]
+            .sudo()
+            .search(
                 [
                     ("company_id", "!=", rec.company_id.id),
                     ("warehouse_id", "=", rec.id),
